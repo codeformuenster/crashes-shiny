2845c201ce32c961a478a67047cdf5cc501d737a
Thomas Kluth <t.kluth@posteo.de>
2018-09-18 22:07:42 +0200

    update texts

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -49,22 +49,22 @@ ui <- navbarPage("Münsteraner Verkehrsunfälle", id = "nav",
     ),
            
 	tabPanel("Rohdaten",
-	         div(h5("instabile development Version, Finger weg!")),
+	         div(h5("Beta-Version, Feedback willkommen!")),
 	         DT::dataTableOutput("accidents_table")
 	         ),
 	
 	tabPanel("Über diese Seite",
-	         div(h5("instabile development Version, Finger weg!")),
+	         div(h5("Beta-Version, Feedback willkommen!")),
 	         h4("Datenquelle"),
-	         p("TODO, Polizei Münster? Lizenz?"),
-	         br(),
-	         a(href = "https://github.com/codeformuenster/open-data/tree/master/Unfallstatistiken", "hier herunterzuladen."),
+	         p("Dank einer Anfrage bei der Polizei Münster über ",
+	           a(href = "https://fragdenstaat.de/anfrage/rohdaten-der-verkehrsunfallstatistik-munster/", target = "_blank", "Frag den Staat"),
+	           "sind wir an Rohdaten der Verkehsunfälle der Jahre 2007 bis 2016 gekommen. Nach mehreren Versuchen ist es uns gelungen, die zwischen den Jahren unterschiedlichen Spaltennamen und Formate in eine Datenbank zu laden um sie zu visualisieren und einfach zugänglich zu machen. Bedauerlicherweise werden die Orte der Verkehrsunfälle in einem schwer maschinenlesbaren Format aufgenommen (z.B.: 'Kappenberger Damm, Höhe Kriegerweg'). Wir haben es uns nun zur Aufgabe gemacht alle Unfälle durchzugehen und den korrekten Ort einzupflegen. Momentan sind weder alle Daten visualisiert noch kann für die Korrektheit der Daten garantiert werden."),
+	         p("Die Rohdaten lassen sich ", a(href = "https://github.com/codeformuenster/open-data/tree/master/Unfallstatistiken", target = "_blank", "hier herunterladen.")),
 	         h4("Lizenz des Quelltexts"),
 	         p("Ein Projekt von ",
-	         	a(href = "https://codeformuenster.org", "Code for Münster"),
-	         	"Lizenziert unter der ",
-	         	a(href = "https://github.com/codeformuenster/accidents#rechtliches", "GPLv3 (mehr Infos zur Lizenz hier)."),
-         	"Ideen und Feedback willkommen!
-Datenquelle: TODO Polizei Münster? Lizenz?")
+	         	a(href = "https://codeformuenster.org", target = "_blank", "Code for Münster")),
+	         p("Lizenziert unter der ",
+	         	a(href = "https://github.com/codeformuenster/accidents#rechtliches", target = "_blank", "GPLv3 (mehr Infos zur Lizenz hier).")),
+	         p("Ideen und Feedback willkommen!")
 	)
 )
