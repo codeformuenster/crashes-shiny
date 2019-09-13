cd2d6d26ac85e310adfe21a57f076a031db564df
Thomas Kluth <t.kluth@posteo.de>
2019-09-13 19:53:52 +0200

    slides RC1

---
+++ doc/vortrag_forum_citizen_science_september_2019/plot1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+rm gnuplot.command
+
+texFile=anzahl-unfaelle-pro-jahr1
+
+mkdir -p img/
+
+echo "set yrange [0:1400]" >> gnuplot.command
+# echo set xrange [0.4:4.6] >> gnuplot.command
+echo "set grid" >> gnuplot.command
+echo "set key box height 0.75 width -2.0 at graph 0.85, 0.3" >> gnuplot.command
+echo "set terminal cairolatex" >> gnuplot.command
+#echo "set title '$dataFile'" >> gnuplot.command
+echo "set ylabel 'Anzahl Unfälle'" >> gnuplot.command
+echo "set output 'img/$texFile.tex'" >> gnuplot.command
+echo "plot 'anzahl-unfaelle-pro-jahr.data' using 1:2 with lines title 'Anzahl Unfälle mit Personenschaden' linetype 1 dashtype 1 linewidth 5" >> gnuplot.command
+
+gnuplot gnuplot.command
+
--- doc/vortrag_forum_citizen_science_september_2019/plot2.sh
+++ doc/vortrag_forum_citizen_science_september_2019/plot2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm gnuplot.command
 
-texFile=anzahl-unfaelle-pro-jahr
+texFile=anzahl-unfaelle-pro-jahr2
 
 mkdir -p img/
 
@@ -14,7 +14,6 @@ echo "set terminal cairolatex" >> gnuplot.command
 echo "set ylabel 'Anzahl Unfälle'" >> gnuplot.command
 echo "set output 'img/$texFile.tex'" >> gnuplot.command
 echo "plot 'anzahl-unfaelle-pro-jahr.data' using 1:2 with lines title 'Anzahl Unfälle mit Personenschaden' linetype 1 dashtype 1 linewidth 5, 'anzahl-unfaelle-pro-jahr.data' using 1:3 with lines title 'Ziel Ordnungspartnerschaft (-10\% pro Jahr)' linetype 2 dashtype 4 linewidth 5" >> gnuplot.command
-#echo pause -1 >> gnuplot.command
 
-gnuplot gnuplot.command 
+gnuplot gnuplot.command
 
